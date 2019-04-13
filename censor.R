3347260e80bba8617f0a6d9e1bd64065427118ca
Eric Novik <eric.novik@gmail.com>
2019-04-13 19:08:09 -0400

    fixes

--- censor.R
+++ censor.R
@@ -11,12 +11,19 @@ cens_idx <- which(y > 1)
 y_cens <- y[y > 1]
 y_obs <- y[-cens_idx]
 
-data <- list(N_obs = length(y_obs), N_cens = length(y_cens), y_obs = y_obs, U = ulq)
+data <-
+  list(
+    N_obs = length(y_obs),
+    N_cens = length(y_cens),
+    y_obs = y_obs,
+    U = ulq
+  )
 
 # mu and sigma estimates with integrated out censor points
-m1 <- stan_model(file = "censor.stan") 
+m1 <- stan_model(file = "censor.stan")
 f1 <- sampling(m1, iter = 300, data = data)
-mean(y); sd(y)
+mean(y)
+sd(y)
 f1
 
 # mu and sigma plus imputed censor points
@@ -28,5 +35,7 @@ f2
 # mu and sigma plus imputed censor points and esitmate the U (ulq)
 m3 <- stan_model(file = "censor_imputed_1.stan")
 f3 <- sampling(m3, iter = 300, data = data)
-mean(y); sd(y); mean(y_cens)
+mean(y)
+sd(y)
+mean(y_cens)
 f3
